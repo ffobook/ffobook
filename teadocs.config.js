5c8f49336fd486993756a39e0ca4f97e73987fd7
rayiooo <rayiooo@foxmail.com>
2019-08-05 15:21:08 +0800

    去掉慢的ga，换用pt

--- teadocs.config.js
+++ teadocs.config.js
@@ -14,7 +14,23 @@ module.exports = {
         dir: "", 
         title: "",
         headHtml: "",
-        footHtml: "",
+        footHtml: `
+			<!-- Ptengine -->
+			<script type="text/javascript">
+				window._pt_lt = new Date().getTime();
+				window._pt_sp_2 = [];
+				_pt_sp_2.push("setAccount,4b8d2226");
+				var _protocol =(("https:" == document.location.protocol) ? " https://" : " http://");
+				(function() {
+					var atag = document.createElement("script");
+					atag.type = "text/javascript";
+					atag.async = true;
+					atag.src = _protocol + "js.ptengine.cn/4b8d2226.js";
+					var s = document.getElementsByTagName("script")[0];
+					s.parentNode.insertBefore(atag, s);
+				})();
+			</script>
+		`,
         isMinify: true,
         rootPath: "/"
     },
